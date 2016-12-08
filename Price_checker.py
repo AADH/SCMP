9b62b0ae72f7a93fd1169aaaab2235070993f495
Abran DeCarlo Hernandez <abranhernandez0@gmail.com>
2016-12-07 19:19:17 -0800

    Update Price_checker.py

--- Price_checker.py
+++ Price_checker.py
@@ -2,4 +2,11 @@
 
 import urllib.request
 
+Team_Fortress_2 = 1
+Counter_Strike_Global_Offensive = 2
+Dota 2 = 3
+
+Print "What game do you want to list?"
+Print "Team Fortress 2 = 1, Counter Strike: Global Offensive = 2, Dota 2 = 3"
+
 page = urllib.request.urlopen("")
