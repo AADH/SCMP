f3c0b643acc2aae42ec250e9a2250ff7f5284c81
Abran DeCarlo Hernandez <abranhernandez0@gmail.com>
2016-12-07 20:38:30 -0800

    Update Price_checker.py

--- Price_checker.py
+++ Price_checker.py
@@ -9,8 +9,11 @@ Team_Fortress_2 = 1
 Counter_Strike_Global_Offensive = 2
 Dota_2 = 3
 
+
 Print "What game do you want to list?"
+while Game_selector = 
 Print "Team Fortress 2 = 1, Counter Strike: Global Offensive = 2, Dota 2 = 3"
+i=input("")
 
 Game_selector = int(i)
 
