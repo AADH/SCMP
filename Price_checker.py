a583b7bc0e2c46402c1f57095dc066817da126a8
Abran DeCarlo Hernandez <abranhernandez0@gmail.com>
2016-12-07 19:23:28 -0800

    Update Price_checker.py

--- Price_checker.py
+++ Price_checker.py
@@ -1,12 +1,17 @@
 #Used to pull prices from the Steam Community Market
+#Code is very messy, will fix later
 
 import urllib.request
 
+Game_selector = 0
+
 Team_Fortress_2 = 1
 Counter_Strike_Global_Offensive = 2
-Dota 2 = 3
+Dota_2 = 3
 
 Print "What game do you want to list?"
 Print "Team Fortress 2 = 1, Counter Strike: Global Offensive = 2, Dota 2 = 3"
+
+Game_selector = int(i)
 
 page = urllib.request.urlopen("")
