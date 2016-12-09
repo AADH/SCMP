3aaf71cc84e973a9fe51cb23b97f7b2f12c8a2f0
Abran DeCarlo Hernandez <abranhernandez0@gmail.com>
2016-12-09 12:19:05 -0800

    Update Main_menu.py

--- Main_menu.py
+++ Main_menu.py
@@ -1,24 +1,24 @@
 #Used to pull prices from the Steam Community Market
-#Code is very messy, will fix later
+#Code is very messy and shitty, will fix later
 
 import urllib.request
-
-Game_selector = 0
 
 Team_Fortress_2 = 1
 Counter_Strike_Global_Offensive = 2
 Dota_2 = 3
 
-
 Print "What game do you want to list?"
 Print "Team Fortress 2 = 1, Counter Strike: Global Offensive = 2, Dota 2 = 3"
 Game_selector = input()
 if Game_selector == 1
+ import TF2_SCM
  elif Game_selector == 2
+  import CSGO_SCM
  elif Game_selector == 3
+  import Dota2_SCM
  elif Game_selector <1
-  print("Please choose a listed game")
+  print("Please input a listed game")
  else Game_selector >3
-  print("Please choose a listed game")
+  print("Please input a listed game")
 
 page = urllib.request.urlopen("")
