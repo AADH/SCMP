f518e553a63919bcb1ff747a2811cde7d214386d
Abran DeCarlo Hernandez <abranhernandez0@gmail.com>
2016-12-09 12:52:29 -0800

    Update Main_menu.py

--- Main_menu.py
+++ Main_menu.py
@@ -10,11 +10,11 @@ Dota_2 = 3
 Print ("What game do you want to list?")
 Print ("Team Fortress 2 = 1, Counter Strike: Global Offensive = 2, Dota 2 = 3")
 Game_selector = input()
-if Game_selector == 1
+if Game_selector == "1":
  import TF2_SCM
- elif Game_selector == 2
+ elif Game_selector == "2":
   import CSGO_SCM
- elif Game_selector == 3
+ elif Game_selector == "3":
   import Dota2_SCM
  elif Game_selector <1
   print("Please input a listed game")
