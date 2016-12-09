3ca0d413ee236da80da39c9140ba0c118bd22478
Abran DeCarlo Hernandez <abranhernandez0@gmail.com>
2016-12-09 14:38:47 -0800

    Update Main_menu.py

--- Main_menu.py
+++ Main_menu.py
@@ -7,18 +7,15 @@ Team_Fortress_2 = 1
 Counter_Strike_Global_Offensive = 2
 Dota_2 = 3
 
-Print ("What game do you want to list?")
-Print ("Team Fortress 2 = 1, Counter Strike: Global Offensive = 2, Dota 2 = 3")
+print ("What game do you want to list?")
+print ("Team Fortress 2 = 1, Counter Strike: Global Offensive = 2, Dota 2 = 3")
 Game_selector = input()
 if Game_selector == "1":
  import TF2_SCM
- elif Game_selector == "2":
+elif Game_selector == "2":
   import CSGO_SCM
- elif Game_selector == "3":
+elif Game_selector == "3":
   import Dota2_SCM
- elif Game_selector <1
-  print("Please input a listed game")
- else Game_selector >3
-  print("Please input a listed game")
+
 
 page = urllib.request.urlopen("")
