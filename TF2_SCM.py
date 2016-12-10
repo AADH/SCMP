3fa0062fa9066ac1ec62b2dacb86ac1bdd069b88
Abran DeCarlo Hernandez <abranhernandez0@gmail.com>
2016-12-10 15:19:19 -0800

    Update TF2_SCM.py
    This version I will try to allow searching for an item by changing the url with the entered string (all SCM games have a specific url to them and has the item in the url)

--- TF2_SCM.py
+++ TF2_SCM.py
@@ -3,3 +3,4 @@ import urllib.request
 page = urllib.request.urlopen("http://steamcommunity.com/market/search?q=&category_440_Type%5B%5D=any&appid=440")
 text = page.read().decode("utf8")
 print(text)
+##########
