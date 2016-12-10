458c5fea65181934ea22a1d1a1e45e37b364ba04
Abran DeCarlo Hernandez <abranhernandez0@gmail.com>
2016-12-09 23:02:41 -0800

    Update Main_menu.py

--- Main_menu.py
+++ Main_menu.py
@@ -1,8 +1,5 @@
 #Used to pull prices from the Steam Community Market
 #Code is very messy and shitty, will fix later
-
-import urllib.request
-
 Team_Fortress_2 = 1
 Counter_Strike_Global_Offensive = 2
 Dota_2 = 3
@@ -16,4 +13,5 @@ elif Game_selector == "2":
   import CSGO_SCM
 elif Game_selector == "3":
   import Dota2_SCM
-else print("Please select a game listed above")
+else:
+    print ("Please select a listed game")
