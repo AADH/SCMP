8db3b8bf8fcc79546613d2cb38fb5c7b20150a21
Abran DeCarlo Hernandez <abranhernandez0@gmail.com>
2016-12-12 09:33:42 -0800

    Update TF2_SCM.py

--- TF2_SCM.py
+++ TF2_SCM.py
@@ -1,5 +1,6 @@
 #Gonna add some TF2 item examples(?)
 import urllib.request
-page = urllib.request.urlopen("http://steamcommunity.com/market/search?q=&category_440_Type%5B%5D=any&appid=440")
+page = urllib.request.urlopen("http://steamcommunity.com/market/listings/440/")
 text = page.read().decode("utf8")
-print(text)
+item = input()
+print (urllib.request.urlopen + input())
